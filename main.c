f3ad8475a19ed291f1e283754cff4295fa3bd07b
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2021-04-07 16:00:46 -0700

    Improve error messages

--- main.c
+++ main.c
@@ -15,11 +15,11 @@ int main(/* int argc, char **argv */) {
     const char *resource = "/sys/bus/pci/devices/0000:01:00.0/resource0";
     int fd = open(resource, O_RDWR);
     if (fd == -1)
-        fatal("open");
+        fatal("Failed to open device");
 
     volatile uint32_t *pfb_fbpa = mmap(NULL, 0x4000, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0x9A0000);
     if (pfb_fbpa == MAP_FAILED)
-        fatal("mmap");
+        fatal("Failed to map device to memory. This can happen if it's locked by the kernel; to run meth-pills, boot your kernel with the iomem=relaxed parameter, which allows userspace access to devices in use by the kernel");
 
 #define NV_PFB_FBPA_MAGIC_1 (0x29c / 4)
 
